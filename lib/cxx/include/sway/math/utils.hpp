537b37540672637199886fa4495bee0446fbb93e
apriori85 <victor-timoshin@hotmail.com>
2024-06-10 00:46:08 +0300

    Fixed

--- lib/cxx/include/sway/math/utils.hpp
+++ lib/cxx/include/sway/math/utils.hpp
@@ -15,7 +15,7 @@ constexpr f32_t PI = 3.14159265358979323846F;
 constexpr f64_t EPSILON = 1.0e-05;  // aka 0.00001
 constexpr s32_t INDEFINITE = -1;
 
-inline auto randomInt(const s32_t nbr) -> std::vector<s32_t> {
+inline auto randomInt(u32_t nbr) -> std::vector<u32_t> {
   auto result = std::vector<u32_t>(nbr);
   for (auto i = 0; i < result.size(); ++i) {
     result.push_back(i);
